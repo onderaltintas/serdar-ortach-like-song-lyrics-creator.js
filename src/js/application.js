decaa72bbb6b95813ba019122c8456af2029a932
Önder <onderaltintas@gmail.com>
2019-05-22 23:05:20 +0300

    Duplication of same words fix
    New words for each sentence. Hope it works. Let's see.

--- src/js/application.js
+++ src/js/application.js
@@ -33,17 +33,21 @@ SongCreator.prototype.createLyrics = function() {
   var firstPart = [];
   var secondPart = [];
   var chorus = [];
-
+  
+  var firstPartClone = firstPart.slice(0);
+  var secondPartClone = firstPart.slice(0);
+  var chorusClone = firstPart.slice(0);
+  
   for (var i = 0; i < 4; i++) {
-    firstPart.push(createSentence(this.firstPartWords));
+    firstPart.push(createSentence(firstPartClone));
   }
 
   for (var i = 0; i < 4; i++) {
-    secondPart.push(createSentence(this.secondPartWords));
+    secondPart.push(createSentence(secondPartClone));
   }
 
   for (var i = 0; i < 2; i++) {
-    chorus.push(createSentence(this.chorusWords));
+    chorus.push(createSentence(chorusClone));
   }
 
   return [firstPart, secondPart, chorus];
@@ -81,7 +85,9 @@ function createSentence(sentenceParts) {
   var sentence = [];
   for (var i = 0; i < sentenceParts.length; i++) {
     var words = sentenceParts[i];
-    var chosenWord = words[getRandom(words.length)];
+    var wordNo = getRandom(words.length);
+    var chosenWord = words[wordNo];
+    words.splice(wordNo, 1);
     sentence.push(chosenWord);
   }
 
